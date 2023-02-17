5fb634512907ce64f1aa6ff8b6e4d6bad6d0a75c
German Rojas <57456438+Gh3rmy@users.noreply.github.com>
2023-02-16 22:51:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 async function getTikTokUserId() {
         const username = document.getElementById('url').value;
         try {
-          const response = await fetch(`https://www.tiktok.com/@${username}?lang=en`, {
+          const response = await fetch(`https://cors-anywhere.herokuapp.com/https://www.tiktok.com/@${username}?lang=en`, {
             headers: {
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
             }
