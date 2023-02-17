ebff54d41ef1f4adb8f0f79c562a5baf357e1975
Gh3rmy <ger_capo_21@hotmail.com>
2023-02-16 22:46:21 -0300

    V.1 Prueba

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# Extract TikTok Datatime (Argentina)
+
+Extract Tiktok Datatime lo que se encarga es de extraer y convertir en horario local de argentina siendo Fecha y Hora local de cualquier video de la plataforma TIKTOK.
+
+## Screenshots
+
+![](http://i.ibb.co/996P3NW/Proyecto-nuevo-6.png)
+
+
+## Demo
+
+https://gh3rmy.github.io/TimeArgentina-TikTok/
+
+
+## Authors
+
+- [@Gh3rmy](https://www.github.com/Gh3rmy)
+
---
+++ js/script.js
@@ -0,0 +1,21 @@
+async function getTikTokUserId() {
+        const username = document.getElementById('url').value;
+        try {
+          const response = await fetch(`https://www.tiktok.com/@${username}?lang=en`, {
+            headers: {
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+            }
+          });
+          const html = await response.text();
+          const regex = /(?<=uniqueId":")(.*?)(?=")/;
+          const match = html.match(regex);
+          if (match) {
+            const userId = match[0];
+            document.getElementById('userId').textContent = userId;
+          } else {
+            throw new Error('Hubo un error al buscar el ID');
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
