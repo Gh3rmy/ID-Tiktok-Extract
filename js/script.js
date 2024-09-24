3a5f65ac3925d05ddda51be0b2c2b633e4aeb0f7
German Rojas <57456438+Gh3rmy@users.noreply.github.com>
2024-09-24 15:32:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ async function getTikTokUserId() {
   try {
       // Cambia el proxy si es necesario
       const response = await fetch(`https://cors-anywhere.herokuapp.com/https://www.tiktok.com/@${username}?lang=en`, {
-          method: 'GET',
           headers: {
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
               'X-Requested-With': 'XMLHttpRequest'
