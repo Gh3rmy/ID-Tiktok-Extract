27074add9b4ff76e1adb8310b17966bf540a7cd0
German Rojas <57456438+Gh3rmy@users.noreply.github.com>
2024-09-24 15:26:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ async function getTikTokUserId() {
       const response = await fetch(`https://cors-proxy.htmldriven.com/?url=https://www.tiktok.com/@${username}?lang=en`, {
           method: 'GET',
           headers: {
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+              'X-Requested-With': 'XMLHttpRequest'
           }
       });
 
